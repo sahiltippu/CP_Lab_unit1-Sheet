c8c941a1cb45b52d0b61667b32b87bd26e7c5ae9
Sahil Tippu M <sahiltippum@gmail.com>
2025-11-12 22:14:48 +0530

    Create P1a.c

---
+++ P1a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number; // Declare an integer variable
+
+    // Ask user for input
+    printf("Enter a number: ");
+    scanf("%d", &number); // Read integer input from user
+
+    // Print the input value back
+    printf("You entered: %d\n", number);
+
+    return 0;
+}
